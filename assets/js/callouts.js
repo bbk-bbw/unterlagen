385e06945e8d7d9a877a3aae5fe1a6fae2285b68
aburossi <142565006+aburossi@users.noreply.github.com>
2024-09-08 11:30:48 +0200

    Update callouts.js

--- assets/js/callouts.js
+++ assets/js/callouts.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const callouts = document.querySelectorAll('blockquote');
   callouts.forEach(function(callout) {
     const firstLine = callout.innerHTML.split('\n')[0];
-    if (firstLine.match(/\[!(\w+)\]/)) {
-      const type = firstLine.match(/\[!(\w+)\]/)[1].toLowerCase();
+    const match = firstLine.match(/\[!(\w+)\]/);
+    if (match) {
+      const type = match[1].toLowerCase();
       callout.classList.add('callout', `callout-${type}`);
-      callout.innerHTML = callout.innerHTML.replace(/\[!(\w+)\]\s*/, '<strong>$1:</strong> ');
+      callout.innerHTML = callout.innerHTML.replace(/\[!(\w+)\]\s*/, '<strong>' + match[1].toUpperCase() + ':</strong> ');
     }
   });
 });
